ffe76037efaf1c3e79081095b88bc45b7bd5733a
Woody <woodkill@gmail.com>
2023-02-22 02:17:35 +0900

    home to office

--- const_key.py
+++ const_key.py
@@ -3,6 +3,7 @@
 """
 kCOL_ODINDATA = "OdinData"
 kCOL_ODINGUILD = u'OdinGuild'
+kCOL_BOSS = u'Boss'
 
 """
  document key
@@ -14,6 +15,8 @@ kDOC_ODINDATA = u'OdinData'
 """
 kFLD_SERVER_LIST = u'serverList'
 kFLD_BOSS_DIC = u'bossDictionary'
+kFLD_SERVER_NAME = u'serverName'
+kFLD_GUILD_NAME = u'guildName'
 
 """
  dictionary key
@@ -24,3 +27,4 @@ kBOSS_ORDER = u'bossOrder'
 kBOSS_NAME = u'bossName'
 kBOSS_ALIAS = u'bossAlias'
 kBOSS_INTERVAL = u'interval'
+
