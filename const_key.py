53bb82febe3f7cf6a306abd92e77579e55b45468
Woody <woodkill@gmail.com>
2023-02-22 02:08:38 +0900

    const_data, const_key 분리

---
+++ const_key.py
@@ -0,0 +1,26 @@
+"""
+ collection key
+"""
+kCOL_ODINDATA = "OdinData"
+kCOL_ODINGUILD = u'OdinGuild'
+
+"""
+ document key
+"""
+kDOC_ODINDATA = u'OdinData'
+
+"""
+ field name
+"""
+kFLD_SERVER_LIST = u'serverList'
+kFLD_BOSS_DIC = u'bossDictionary'
+
+"""
+ dictionary key
+"""
+kCHAP_NO = u'chapNumber'
+kBOSS_LEVEL = u'bossLevel'
+kBOSS_ORDER = u'bossOrder'
+kBOSS_NAME = u'bossName'
+kBOSS_ALIAS = u'bossAlias'
+kBOSS_INTERVAL = u'interval'
