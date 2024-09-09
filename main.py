2be975f856289b4a8a32368084688e65e75877e7
Woody Moon <woodkill@gmail.com>
2024-09-09 19:47:54 +0900

    ..

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def upload_json_boss_dic_fo_firestore(filepath: str):
 # make_json_server_dic_from_firestore(cSERVER_DICT_JSON_FILEPATH)
 # upload_json_server_dic_fo_firestore(cSERVER_DICT_JSON_FILEPATH)
 # reset_boss_dic()
-# make_json_boss_dic_from_firestore(cBOSS_DICT_JSON_FILEPATH)
+make_json_boss_dic_from_firestore(cBOSS_DICT_JSON_FILEPATH)
 # upload_json_boss_dic_fo_firestore(cBOSS_DICT_JSON_FILEPATH)
 
 # humanize.i18n.activate("ko_KR")
